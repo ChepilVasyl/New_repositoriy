c64073ba86464ce7b70fd43f2a30b954434606ae
mchepil <mchepil@mint-innovations.com>
2023-06-04 12:10:18 +0300

    Perevirka na rozuminnya

--- Practic_CPP/Practic_CPP/Practic_CPP.cpp
+++ Practic_CPP/Practic_CPP/Practic_CPP.cpp
@@ -14,12 +14,7 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int a, b, c;
-    cout << "Введіть сторону а" << endl;
-    cin >> a;
-    cout << "Введіть сторону б" << endl;
-    cin >> b;
-    cout << "Проща квадрата = "<< Square_area(a,b) << endl;
+    int a;
     cout << "Введіть число для визначення його куба" << endl;
     cout << "Введіть число" << endl;
     cin >> a;
